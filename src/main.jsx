ee6e8fb85b34c6fa968b7061d136b3b58c620b3b
Wilpost <wilyerqueiporiera@gmail.com>
2024-06-06 21:28:30 -0400

    Fix bug

--- src/main.jsx
+++ src/main.jsx
@@ -11,16 +11,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Buffer } from 'buffer'
 window.Buffer = Buffer
 
-const queryClient = new QueryClient()
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </BrowserRouter>
 )
